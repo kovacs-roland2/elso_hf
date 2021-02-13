f4bd7571aff4bd945cf22ef5fa36998aa056666d
Roland Kovacs <koviroli96@gmail.com>
2021-02-13 17:21:49 +0100

    memoria hasznalata

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,14 @@
 def fibonacci(n: int, cache: list= [1, 1]): 
-    cache = [1,1]
-    for i in range(n - 2):
-        cache.append(cache[i] + cache[i+1])
-    print(f'The {n}th number in the Fibonacci series is {cache[-1]}.')    
+    #print(cache)
+    current_len = len(cache)
+    if current_len < n:
+        for i in range(n - current_len):
+            cache.append(cache[-2] + cache[-1])
+        to_print = cache[-1]
+    else:
+        to_print = cache[n]
+      
+    #print(cache)
+    print(f'The {n}th number in the Fibonacci series is {to_print}.') 
     
 fibonacci(4)
